38f1e2f9c7f3421ba8f47638c09f74226234a0b4
jepaki <jepaki@mtu.edu>
2024-04-18 15:02:27 -0400

    Refreshing myself on the code

--- scripts/ufm_raw_plotter.py
+++ scripts/ufm_raw_plotter.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 ufm_raw_data_csv = Path("/home/jepaki/Projects/Objects/FortStewart2024/RadiometerTesting/datalog5_output/DATLOG5_2024-02-10T13:34:52+00:00.csv")
 ufm_raw_data_csv = Path("/home/jepaki/Projects/Objects/FortStewart2024/RadiometerTesting/datalog3_output/DATLOG3_2024-02-10T13:59:25+00:00.csv")
-ufm_raw_data_csv = Path("/home/jepaki/Projects/Objects/FortStewart2024/RadiometerTesting/datalog6_output/DATLOG6_2024-02-10T13-43-34+00-00.csv")
+ufm_raw_data_csv = Path("/home/oryx/Projects/Objects/FortStewart2024/RadiometerTesting/datalog6_output/DATLOG6_2024-02-10T13-43-34+00-00.csv")
 ufm_df = pd.read_csv(ufm_raw_data_csv)
 print(ufm_df.head())
 print(ufm_df.columns)
@@ -21,6 +21,36 @@ axs[0,0].legend()
 axs[0,1].legend()
 axs[1,0].legend()
 axs[1,1].legend()
+# xmin=8500
+# xmax=9400
+# axs[0,0].set_xlim(xmin, xmax)
+# axs[0,1].set_xlim(xmin, xmax)
+# axs[1,0].set_xlim(xmin, xmax)
+# axs[1,1].set_xlim(xmin, xmax)
 plt.savefig(ufm_raw_data_csv.parent.joinpath(ufm_raw_data_csv.stem + ".png"))
+
+fig, axs = plt.subplots(2, 1, figsize=(5, 8))
+x_peak = np.argmax(ufm_df["WIDE"].astype('float'))
+axs[0].plot(ufm_df["WIDE"].astype('float'), label="WIDE")
+axs[0].axvline(x_peak, color='r', linestyle='--')
+pressure_diff = ufm_df["Flow"].astype('float')
+N=10
+pressure_diff_ave = np.convolve(pressure_diff , np.ones(N)/N, mode='valid')
+axs[1].plot(pressure_diff, label="Flow")
+axs[1].plot(pressure_diff_ave, label="10s Average")
+axs[1].axvline(x_peak, color='r', linestyle='--')
+axs[0].set_title("Wideband Radiometric Signal")
+axs[1].set_title("Differential Pressure Signal")
+axs[1].legend()
+xmin=8800
+xmax=9200
+axs[0].set_xlim(xmin, xmax)
+axs[1].set_xlim(xmin, xmax)
+axs[1].set_xlabel("Sample (1 Hz)")
+axs[0].set_ylabel("Voltage (mV)")
+axs[1].set_ylabel("Voltage (mV)")
+plt.savefig(ufm_raw_data_csv.parent.joinpath(ufm_raw_data_csv.stem + "_wide.png"))
+plt.show()
+
 plt.show()
 
